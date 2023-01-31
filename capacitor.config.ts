34083d9950c5d8a8dca6adf1f420388ec687a235
chrisnuval <christian.nuval@xendit.co>
2023-01-31 17:04:44 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.pasabuy',
+  appName: 'pasabuy',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
